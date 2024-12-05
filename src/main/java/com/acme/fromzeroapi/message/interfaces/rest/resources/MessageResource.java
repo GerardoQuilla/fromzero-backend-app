b0e1954bdc9ae4c38fa365c752d6ad4c4f4ff351
GerardoQuilla <g.quillaluyo@gmail.com>
2024-12-05 13:13:49 -0500

    hotfix(message): response fixed

--- src/main/java/com/acme/fromzeroapi/message/interfaces/rest/resources/MessageResource.java
+++ src/main/java/com/acme/fromzeroapi/message/interfaces/rest/resources/MessageResource.java
@@ -1,8 +1,5 @@
 package com.acme.fromzeroapi.message.interfaces.rest.resources;
 
-import com.acme.fromzeroapi.message.domain.model.aggregates.Chat;
-
-import java.time.LocalDate;
 import java.util.Date;
 
 public record MessageResource(
