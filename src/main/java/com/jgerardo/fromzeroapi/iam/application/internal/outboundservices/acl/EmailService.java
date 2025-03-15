0f0aa096c7fef54687aad3cfabb24ff86762f798
GerardoQuilla <g.quillaluyo@gmail.com>
2025-03-14 19:54:07 -0500

    refactor(emailService): exceptions added
    se agregaron excepciones
    se agrego timeout para esperar respuesta del servidor smtp

--- src/main/java/com/jgerardo/fromzeroapi/iam/application/internal/outboundservices/acl/EmailService.java
+++ src/main/java/com/jgerardo/fromzeroapi/iam/application/internal/outboundservices/acl/EmailService.java
@@ -2,13 +2,17 @@ package com.jgerardo.fromzeroapi.iam.application.internal.outboundservices.acl;
 
 import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.angus.mail.util.MailConnectException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -30,7 +34,7 @@ public class EmailService {
     }
 
     @Async
-    public void sendEmail(String username, String token) {
+    public CompletableFuture<Void> sendEmail(String username, String token) {
         Context context = new Context();
         context.setVariable("verificationUrl", verificationUrl+token);
         context.setVariable("name", username);
@@ -46,8 +50,18 @@ public class EmailService {
 
             mailSender.send(mimeMessage);
             log.info("Email sent to {}", username);
+
+            return CompletableFuture.completedFuture(null);
+
+        }catch (MailAuthenticationException e){
+            log.error("Email authentication failed. {}", e.getMessage());
+            return CompletableFuture.failedFuture(e);
+        } catch (MailConnectException e) {
+            log.error("Could not connect to SMTP server. {}", e.getMessage());
+            return CompletableFuture.failedFuture(e);
         } catch (MessagingException e) {
-            log.error("Error during email sending.{}", e.getMessage());
+            log.error("Error during email sending. {}", e.getMessage());
+            return CompletableFuture.failedFuture(e);
         }
     }
 
